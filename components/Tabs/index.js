73f800aa77134ba1b5d6c02f99d230fa7ea3133e
Sigi Jaeckel <sigij55@gmail.com>
2020-06-05 11:56:59 -0400

    working on mvp task 2

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,20 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response =>{
+        const topicArray = response.data.topics
+        console.log(topicArray)
+
+        topicArray.forEach(topic =>{
+            const newTab = document.createElement('div')
+            const tabSection = document.querySelector('div.topics')
+            newTab.classList.add('tab')
+            newTab.textContent = topic
+            console.log(newTab)
+
+            tabSection.appendChild(newTab)
+
+        })
+    })
